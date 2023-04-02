f3eb9d26ff55dfc04ad9b836a4350371c6a66383
TeaSpillStudios <teaspillstudios@proton.me>
2023-04-02 14:14:17 +0100

    Add basic code to recieve messages from chat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use tokio::{task, time};
+use youtube_chat::{item::ChatItem, live_chat::LiveChatClientBuilder};
+
+#[tokio::main]
+async fn main() {
+    let mut client = LiveChatClientBuilder::new()
+        .url("https://www.youtube.com/watch?v=jfKfPfyJRdk".to_string())
+        .unwrap()
+        .on_chat(|chat_item| handle_message(chat_item))
+        .on_error(|error| println!("{error}"))
+        .build();
+
+    client.start().await.unwrap();
+    let forever = task::spawn(async move {
+        let mut interval = time::interval(Duration::from_millis(3000));
+        loop {
+            interval.tick().await;
+            client.execute().await;
+        }
+    });
+
+    forever.await.unwrap();
+}
+
+fn handle_message(item: ChatItem) {
+    println!("{:?}", item.message);
 }
